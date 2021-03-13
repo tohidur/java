d591298add9a4b2839fd5ba63183f318937e310b
tohidur <tohidur@urbanpiper.com>
2021-03-13 17:36:30 +0530

    width of binary tree

--- problems/leet_code/39_m_WidthOfBinaryTree.java
+++ problems/leet_code/39_m_WidthOfBinaryTree.java
@@ -1,4 +1,6 @@
 import java.util.HashMap;
+import java.util.Queue;
+import java.util.LinkedList;
 
 class TreeNode {
     int val;
@@ -9,19 +11,58 @@ class TreeNode {
 
 class Solution {
     int max_width;
-    HashMap<Integer, Integer> lvl_width;
+    HashMap<Integer, Integer> leftmost_positions;
 
     public int widthOfBinaryTree(TreeNode root) {
-        lvl_width = new HashMap<>();
-        get_max_width(root, 0, lvl_width);
+        max_width = 0;
+        leftmost_positions = new HashMap<>();
+        get_width(root, 0, 0);
         return max_width;
     }
 
-    private void get_max_width(TreeNode root, int lvl, HashMap<Integer, Integer> lvl_width) {
+    public void get_width(TreeNode root, int depth, int position) {
         if (root == null) return;
+        leftmost_positions.computeIfAbsent(depth, x->position);
+        max_width = Math.max(max_width, position - leftmost_positions.get(depth)+1);
+        get_width(root.left, depth+1, position*2);
+        get_width(root.right, depth+1, position*2+1);
+    }
 
-        int cur = lvl_width.getOrDefault(lvl, 0);
-        lvl_width.put(lvl, cur+1);
+    // Width of binary tree iterative - Error
+    private class QE {
+        TreeNode node;
+        int lvl;
+        int pos;
+        QE(TreeNode n, int l, int p) {
+            node = n;
+            pos = p;
+            lvl = l;
+        }
+    }
+
+    public int widthOfBinaryTreeItr(TreeNode root) {
+        if (root == null) return 0;
+
+        int max_pos = 0;
+        Queue<QE> q = new LinkedList<>();
+        q.offer(new QE(root, 0, 0));
+
+        while (!q.isEmpty()) {
+            QE curQE = q.remove();
+
+            if (curQE.node.left != null) {
+                int lpos = 2 * curQE.pos + 1;
+                q.offer(new QE(curQE.node.left, curQE.lvl+1, lpos));
+            }
+            if (curQE.node.right != null) {
+                int rpos = 2 * curQE.pos + 2;
+                q.offer(new QE(curQE.node.right, curQE.lvl+2, rpos));
+            }
+            int first_l = (int)(Math.pow(2, (curQE.pos-1)) - 1);
+            max_pos = Math.max(max_pos, Math.abs(curQE.pos - first_l) - 1);
+        }
+
+        return max_pos;
     }
 
     public static void main(String args[]) {
