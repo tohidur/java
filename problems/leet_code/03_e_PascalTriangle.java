bd317bdb4d2c48e8c377e51cf6e1798fa6e1b315
tohidur <tohidur@urbanpiper.com>
2021-07-25 10:43:25 +0530

    explain question pascal's triangle

--- problems/leet_code/03_e_PascalTriangle.java
+++ problems/leet_code/03_e_PascalTriangle.java
@@ -1,3 +1,19 @@
+/* Pascal's Triangle
+ *
+ * Given a non-negative integer numRows, generate the first numRows of pascals
+ * triangle.
+ *
+ * Input: 5
+ * Output: 
+ * [
+ *      [1]
+ *     [1, 1]
+ *    [1, 2, 1]
+ *  [1, 3,  3, 1]
+ * ]
+ */
+
+
 import java.io.*;
 import java.util.*;
 
